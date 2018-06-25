c3592891854d6b7e4a8d51a78f4035014d79048c
zhao-db <40554374+zhao-db@users.noreply.github.com>
2018-06-25 15:20:52 +0800

    update

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		String string = "HelloWorld!";
+		String string = "HelloWorld111!";
 		System.out.println(string);
 		
 	}
